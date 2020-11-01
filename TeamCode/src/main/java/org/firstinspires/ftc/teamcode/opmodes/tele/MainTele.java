23370b32fe83fb6cb41c6b4a8d21244624e2cf3e
dgorbunov <38931252+dgorbunov@users.noreply.github.com>
2020-11-01 17:37:52 -0500

    Add shooter velocity control

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/tele/MainTele.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/tele/MainTele.java
@@ -5,9 +5,11 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.robotcore.internal.android.dx.command.Main;
 import org.firstinspires.ftc.teamcode.robot.Controller;
 import org.firstinspires.ftc.teamcode.robot.drive.DrivetrainController;
 import org.firstinspires.ftc.teamcode.robot.mech.IntakeController;
+import org.firstinspires.ftc.teamcode.robot.mech.ShooterController;
 import org.openftc.revextensions2.ExpansionHubEx;
 
 import java.lang.reflect.Constructor;
@@ -22,6 +24,8 @@ public class MainTele extends OpMode {
 
     private DrivetrainController drive;
     private IntakeController intake;
+    private ShooterController shooter;
+
     private List<Controller> controllers = new ArrayList<Controller>();
 
     public static Telemetry sTelemetry;
@@ -32,17 +36,21 @@ public class MainTele extends OpMode {
 
     public void init() {
         if (noChassis) {
-        //    drive = new mockController;
-        } else drive = new DrivetrainController(
-                hardwareMap.get(DcMotor.class, "left_front"),
-                hardwareMap.get(DcMotor.class, "left_rear"),
-                hardwareMap.get(DcMotor.class, "right_front"),
-                hardwareMap.get(DcMotor.class, "right_rear"),
-                hardwareMap);
+        //    drive = new DrivetrainController(hardwareMap.get(MockDcMotor.class, ""));
+        } else {
+            drive = new DrivetrainController(
+                    hardwareMap.get(DcMotor.class, "left_front"),
+                    hardwareMap.get(DcMotor.class, "left_rear"),
+                    hardwareMap.get(DcMotor.class, "right_front"),
+                    hardwareMap.get(DcMotor.class, "right_rear"),
+                    hardwareMap);
+            shooter = new ShooterController(hardwareMap.get(DcMotor.class, "shooter"), hardwareMap);
+        }
 
 
         controllers.add(drive);
         controllers.add(intake);
+        controllers.add(shooter);
         for (Controller c:controllers) {
             c.init();
         }
@@ -50,7 +58,6 @@ public class MainTele extends OpMode {
     }
 
     public void init_loop() {
-        //drive.setPower();
     }
 
     public void start() { //code to run once when play is hit
@@ -59,13 +66,16 @@ public class MainTele extends OpMode {
     public void loop() {
 
         drive.drive(gamepad1);
-        telemetry.addLine("Using " + Hub.getTotalModuleCurrentDraw(ExpansionHubEx.CurrentDrawUnits.AMPS) + "A");
+        shooter.shoot(gamepad1, gamepad1.a);
 
+        telemetry.addLine("Using " + Hub.getTotalModuleCurrentDraw(ExpansionHubEx.CurrentDrawUnits.AMPS) + "A");
     }
 
     public void stop() { //code to run when program is stopped
         telemetry.addLine("Stopping Program");
-        drive.stop();
+        for (Controller c:controllers) {
+            c.stop();
+        }
     }
 
     public static Telemetry GetTelemetry() {
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/mech/ShooterController.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/mech/ShooterController.java
@@ -1,11 +1,22 @@
 package org.firstinspires.ftc.teamcode.robot.mech;
 
+import com.acmerobotics.roadrunner.util.Angle;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.Gamepad;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.teamcode.opmodes.tele.MainTele;
 import org.firstinspires.ftc.teamcode.robot.Controller;
 
 public class ShooterController implements Controller {
+
+    float wheelRadius = 0.051f; //meters
+
+    Telemetry telemetry = MainTele.GetTelemetry();
+    DcMotorEx shooter;
 
     public ShooterController (DcMotor shooter,
                               HardwareMap hardwareMap){
@@ -14,7 +25,18 @@ public class ShooterController implements Controller {
 
     @Override
     public void init() {
+        shooter.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+    }
 
+    public void shoot(Gamepad gamepad, boolean button){
+        if (button){
+            shooter.setPower(1);
+        } else stop();
+
+        String velocity = shooter.getVelocity(AngleUnit.RADIANS) + " rad/s";
+        String velocityTangential = shooter.getVelocity(AngleUnit.RADIANS) * wheelRadius + " m/s";
+        telemetry.addData("Shooter Velocity:", velocity);
+        telemetry.addData("Shooter Velocity:", velocityTangential); //v = r*w
     }
 
     @Override
@@ -24,6 +46,6 @@ public class ShooterController implements Controller {
 
     @Override
     public void stop() {
-
+        shooter.setPower(0);
     }
 }
