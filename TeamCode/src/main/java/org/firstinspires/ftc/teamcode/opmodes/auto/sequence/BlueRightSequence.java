5ad8ba3a4cafa46cf7c30a0ee0bf0fa05f7daffd
dannygorbunov <38931252+dgorbunov@users.noreply.github.com>
2020-11-23 19:48:21 -0800

    Fixed init().

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/auto/sequence/BlueRightSequence.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/auto/sequence/BlueRightSequence.java
@@ -1,7 +1,6 @@
 package org.firstinspires.ftc.teamcode.opmodes.auto.sequence;
 
 import com.acmerobotics.roadrunner.geometry.Pose2d;
-import com.qualcomm.robotcore.hardware.HardwareMap;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 import org.firstinspires.ftc.teamcode.opmodes.auto.Constants;
@@ -9,18 +8,11 @@ import org.firstinspires.ftc.teamcode.robot.ControllerManager;
 
 public class BlueRightSequence extends Sequence {
 
-    public BlueRightSequence(ControllerManager controllers, HardwareMap hwMap, Telemetry tel) {
+    public BlueRightSequence(ControllerManager controllers, Telemetry tel) {
         super(controllers, tel);
-    }
-
-    public void init() {
-        Pose2d startPose = new Pose2d(
+        this.startPose = new Pose2d(
                 Constants.BlueRight.StartingPos,
                 Math.toRadians(0)); // TODO: rotate 180?
-
-        super.init(startPose);
-
-        makeActions();
     }
 
     protected void makeActions() {
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/auto/sequence/ExampleSequence.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/auto/sequence/ExampleSequence.java
@@ -1,25 +1,21 @@
 package org.firstinspires.ftc.teamcode.opmodes.auto.sequence;
 
-import com.qualcomm.robotcore.hardware.HardwareMap;
-
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 import org.firstinspires.ftc.teamcode.robot.ControllerManager;
 
 public class ExampleSequence extends Sequence {
-    public ExampleSequence(ControllerManager controllers, HardwareMap hwMap, Telemetry tel){
+    public ExampleSequence(ControllerManager controllers, Telemetry tel){
         super(controllers, tel);
     }
 
     protected void makeActions() {
-//        actions.addAction(() -> moveToZone());
-        actions.add(() -> dropWobble());
-        actions.add(() -> moveToStart());
-        actions.add(() -> collectWobble());
-
-        actions.add(() -> intakeRings());
-
-        actions.add(() -> moveToShoot());
-        actions.add(() -> shootRings());
-        actions.add(() -> moveToLaunchLine());
+//        actions.addAction(this::moveToZone);
+        actions.add(this::dropWobble);
+        actions.add(this::moveToStart);
+        actions.add(this::collectWobble);
+        actions.add(this::intakeRings);
+        actions.add(this::moveToShoot);
+        actions.add(this::shootRings);
+        actions.add(this::moveToLaunchLine);
     }
 }
