8d8c20f11bb2512568a535fc863cebb235b0e314
dannygorbunov <38931252+dgorbunov@users.noreply.github.com>
2020-11-29 11:22:01 -0800

    Check ringCount.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/auto/FullAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/auto/FullAuto.java
@@ -61,9 +61,10 @@ public class FullAuto extends OpMode {
         // TODO: use different opmodes for alliance, side. For now, we are assuming Red Alliance, Left side:
         String sequenceName = makeSequenceName(Constants.RedAlliance, Constants.LeftSide);
         synchronized (lock) {
-            // TODO: check eventually
             if (ringCount == -1) {
-                telemetry.addLine("Camera found 0 rings!");
+                telemetry.addLine("Invalid ringCount!");
+                // TODO: check and return here eventually
+                // return;
             }
 
             currentSequence = getSequence(sequenceName);
